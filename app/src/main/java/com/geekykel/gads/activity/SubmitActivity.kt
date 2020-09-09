e96ff9f72dd4776f77b3f0ea29c5acaa096fdaf9
isievwore <isievwore@systemspecs.com.ng>
2020-09-09 23:38:23 +0100

    Added Submit Activity

---
+++ app/src/main/java/com/geekykel/gads/activity/SubmitActivity.kt
@@ -0,0 +1,12 @@
+package com.geekykel.gads.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SubmitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_submit)
+    }
+}
