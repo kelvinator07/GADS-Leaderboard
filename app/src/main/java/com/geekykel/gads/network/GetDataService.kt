68e453ba18585c76ddae89eedcd1af6b2931cd43
isievwore <isievwore@systemspecs.com.ng>
2020-09-09 23:52:30 +0100

    Implemented Retrofit

--- app/src/main/java/com/geekykel/gads/network/GetDataService.kt
+++ app/src/main/java/com/geekykel/gads/network/GetDataService.kt
@@ -1,13 +1,26 @@
 package com.geekykel.gads.network
 
-import com.geekykel.gads.model.DataModel
+import com.noha.gadsleaderboard.model.LearnerModel
 import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface GetDataService {
+
     @GET("/api/hours")
-    fun getHours(): Call<List<DataModel>>
+    fun getHours(): Call<List<LearnerModel>>
 
     @GET("/api/skilliq")
-    fun getSkilliq(): Call<List<DataModel>>
+    fun getSkilliq(): Call<List<LearnerModel>>
+
+    @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
+    @FormUrlEncoded
+    fun submitProject(
+        @Field("entry.1877115667") firstName: String,
+        @Field("entry.2006916086") lastName: String,
+        @Field("entry.1824927963") emailAddress: String,
+        @Field("entry.284483984") githubLink: String
+    ): Call<Void>
 }
--- app/src/main/java/com/geekykel/gads/network/RetrofitClientInstance.kt
+++ app/src/main/java/com/geekykel/gads/network/RetrofitClientInstance.kt
@@ -1,7 +1,10 @@
 package com.geekykel.gads.network
 
+import com.google.gson.Gson
+import com.noha.gadsleaderboard.model.ErrorResponse
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.HttpException
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,19 +14,77 @@ object RetrofitClientInstance {
         this.level = HttpLoggingInterceptor.Level.BODY
     }
 
-    private val client = OkHttpClient.Builder()
-        .addInterceptor(interceptor)
-        .build()
+    private val client by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+    }
 
-    private val BASE_URL: String = "https://gadsapi.herokuapp.com/" // hours
+    private const val BASE_URL: String = "https://gadsapi.herokuapp.com"
+    private const val GOOGLE_SHEET_URL: String = "https://docs.google.com"
 
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .client(client)
-        .build()
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            //.addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    private val googleSheetRetrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(GOOGLE_SHEET_URL)
+            //.addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
 
     fun<T> buildService(service: Class<T>): T {
         return retrofit.create(service)
     }
+
+    fun convertErrorBody(throwable: HttpException): ErrorResponse? {
+        return try {
+
+            throwable.response()?.errorBody()?.let {
+                val gson = Gson()
+                val error: ErrorResponse = gson.fromJson(
+                    it.string(),
+                    ErrorResponse::class.java
+                )
+                error
+            }
+
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+            null
+        }
+    }
 }
+
+//
+//suspend fun <T> safeApiCall(
+//    dispatcher: CoroutineDispatcher,
+//    apiCall: suspend () -> Deferred<T>
+//): ResultWrapper<T> {
+//    return withContext(dispatcher) {
+//        try {
+//            ResultWrapper.Success(apiCall.invoke().await())
+//        } catch (throwable: Throwable) {
+//            when (throwable) {
+//                is HttpException -> {
+//                    val code = throwable.code()
+//                    val errorResponse = convertErrorBody(throwable)
+//                    ResultWrapper.GenericError(code, errorResponse)
+//                }
+//                is IOException -> ResultWrapper.NetworkError
+//                else -> {
+//                    ResultWrapper.GenericError(null, null)
+//                }
+//            }
+//        }
+//    }
+//}
+
